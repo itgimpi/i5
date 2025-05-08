ced8ff6cb0a1faeb861f83bcbf5d538806be1265
itgimpi <brejkersi@gmail.com>
2025-05-08 09:50:59 +0200

    Update t1b.c

--- 12ui/t1b.c
+++ 12ui/t1b.c
@@ -7,8 +7,7 @@ int main() {
     FILE *f;
     f = fopen("test1.txt", "r"); // otvori fajl za citanje...
 
-    //srand(time(NULL));
-    
+  
     for (int i = 0; i < 1000; i++)  { // 1000 puta
         int c = getc(f); // uzmi 1 znak iz fajla
         putchar(c); // i posalji ga na standardni izlaz
